b7398328fe27f00098900757534b8ee3017d2a50
Ethan Crompton <ethan.crompton1@gmail.com>
2022-03-18 10:04:46 +0000

    SUmmary
    reworking the script to be dynamic.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var audioClick = new Audio('assets/single-click.wav');
 let maxGuesses = 5;
 let curGuesses = 0;
 let prevGuesses = [];
@@ -31,45 +32,60 @@ let answer = "hello";
 //   console.log(movieArr);
 // }
 
-let l1 = document.getElementById("l1");
-let l2 = document.getElementById("l2");
-let l3 = document.getElementById("l3");
-let l4 = document.getElementById("l4");
-let l5 = document.getElementById("l5");
+// let l1 = document.getElementById("l1");
+// let l2 = document.getElementById("l2");
+// let l3 = document.getElementById("l3");
+// let l4 = document.getElementById("l4");
+// let l5 = document.getElementById("l5");
 
-document.onkeydown = (e) => {
-
-  // Backspace check
-  if(e.key == "Backspace"){
-    if(l5.innerText.length > 0){
-      l5.innerText = "";
-    } else if(l4.innerText.length > 0){
-      l4.innerText = "";
-    } else if(l3.innerText.length > 0){
-      l3.innerText = "";
-    } else if(l2.innerText.length > 0){
-      l2.innerText = "";
-    } else if(l1.innerText.length > 0){
-      l1.innerText = "";
-    }
-  } else if(e.key >= "a" && e.key <= "z"){
-    if(l1.innerText.length < 1){
-      l1.innerText = e.key;
-    } else if(l2.innerText.length < 1){
-      l2.innerText = e.key;
-    } else if(l3.innerText.length < 1){
-      l3.innerText = e.key;
-    } else if(l4.innerText.length < 1){
-      l4.innerText = e.key;
-    } else if(l5.innerText.length < 1){
-      l5.innerText = e.key;
-    } 
-  } else if(e.key = "Enter"){
-    var word = l1.innerText + l2.innerText + l3.innerText + l4.innerText + l5.innerText;
-    if (word.length == 5){
-      checkAnswer(word);
+document.onkeyup = (e) => {
+  for(var i = 0; i < answer.length; i++){
+    if(e.key == "Backspace"){
+      if (document.getElementById("l" + i).innerText.length > 0){
+        document.getElementById("l" + i).innerText = "";
+        break;
+      }
+    } else if(e.key >= "a" && e.key <= "z"){
+      if(document.getElementById("l" + i).innerText.length < 1){
+        document.getElementById("l" + i).innerText = e.key;
+        break;
+      }
+    } else if(e.key = "Enter"){
+      var word = word + document.getElementById("l" + i).innerText;
     }
   }
+
+  // // Backspace check
+  // if(e.key == "Backspace"){
+  //   if(l5.innerText.length > 0){
+  //     l5.innerText = "";
+  //   } else if(l4.innerText.length > 0){
+  //     l4.innerText = "";
+  //   } else if(l3.innerText.length > 0){
+  //     l3.innerText = "";
+  //   } else if(l2.innerText.length > 0){
+  //     l2.innerText = "";
+  //   } else if(l1.innerText.length > 0){
+  //     l1.innerText = "";
+  //   }
+  // } else if(e.key >= "a" && e.key <= "z"){
+  //   if(l1.innerText.length < 1){
+  //     l1.innerText = e.key;
+  //   } else if(l2.innerText.length < 1){
+  //     l2.innerText = e.key;
+  //   } else if(l3.innerText.length < 1){
+  //     l3.innerText = e.key;
+  //   } else if(l4.innerText.length < 1){
+  //     l4.innerText = e.key;
+  //   } else if(l5.innerText.length < 1){
+  //     l5.innerText = e.key;
+  //   } 
+  // } else if(e.key = "Enter"){
+  //   var word = l1.innerText + l2.innerText + l3.innerText + l4.innerText + l5.innerText;
+  //   if (word.length == 5){
+  //     checkAnswer(word);
+  //   }
+  // }
   //alert(e.key)
 
   function checkAnswer(word){
@@ -79,3 +95,12 @@ document.onkeydown = (e) => {
 		}
 	}
 }
+
+function createWord(){
+  var inputs = document.getElementById("inputs");
+  for (let i = 0; i < answer.length; i++){
+    inputs.insertAdjacentHTML('beforeend', '<div class="inputTest" id="l' + i + '"></div>')
+  }
+}
+
+createWord();
